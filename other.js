5c3c74d9819edff1b659c5a71b0f9c6fee53d096
Mulia Arifandi Nasution <mul14.net@gmail.com>
2018-01-09 14:55:28 +0700

    They want load like old school style

---
+++ other.js
@@ -0,0 +1,11 @@
+var input = document.querySelector('#awesomplete')
+
+var awesomplete = new Awesomplete(input)
+
+awesomplete.list = [
+  'Bakso Solo',
+  'Bakso Enak',
+  'Sate Padang',
+  'Mie Aceh',
+  'Soto Banjar',
+]
--- src/App.vue
+++ src/App.vue
@@ -6,43 +6,16 @@
       <input id="awesomplete"/>
     </p>
 
-    <p>
-      Add new <input type="text" v-model="newValue" @keyup.enter="add">
-      <button @click="add">Add</button>
-    </p>
-
-    <pre v-text="$data"></pre>
+    <div>
+      Available data:
+      <ul>
+        <li>Bakso Solo</li>
+        <li>Bakso Enak</li>
+        <li>Sate Padang</li>
+        <li>Mie Aceh</li>
+        <li>Soto Banja</li>
+      </ul>
+    </div>
 
   </div>
 </template>
-
-<script>
-  import Awesomplete from 'awesomplete'
-
-  export default {
-
-    name: 'AwesompleteExample',
-
-    data () {
-      return {
-        awesomplete: null,
-        newValue: '',
-        list: ['Bakso Solo', 'Bakso Enak', 'Sate Padang', 'Mie Aceh', 'Soto Banjar'],
-      }
-    },
-
-    mounted () {
-      const input = document.querySelector('#awesomplete')
-      this.awesomplete = new Awesomplete(input)
-      this.awesomplete.list = this.list
-    },
-
-    methods: {
-      add () {
-        this.list.push(this.newValue)
-        this.newValue = ''
-      },
-    },
-
-  }
-</script>
